12fb0cfb9b7a7c8ef80c539afabd10a03f2b6949
Laure Runser <laure-runser@tutanota.com>
2021-02-11 20:12:08 +0100

    redo the gradle config for Spring Boot

---
+++ server/src/main/java/fr/laurerunser/discool/discool/DiscoolApplication.java
@@ -0,0 +1,13 @@
+package fr.laurerunser.discool.discool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoolApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/fr/laurerunser/discool/discool/DiscoolApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.laurerunser.discool.discool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
