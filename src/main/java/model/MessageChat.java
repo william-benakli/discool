4c568a8deaffe4b5ab9acf12be8243d3cb388068
bouanem <bouanemyani2018@gmail.com>
2021-02-14 14:34:04 +0100

    creation du model pour les messages de chat

---
+++ src/main/java/model/MessageChat.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+public class MessageChat {
+    @Getter
+    private MessageType type ;
+
+    @Getter
+    private String content ;
+
+    @Getter
+    private String sender ;
+
+    @Getter
+    private String time ;
+}
