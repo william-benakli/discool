22890d93e24c3cf80855f6f48f957f035505908a
benakli <william.benakli.4.2@gmail.com>
2021-02-20 15:46:00 +0100

    Fix bad merge

--- src/main/java/app/web/TextChannelView.java
+++ src/main/java/app/web/TextChannelView.java
@@ -1,11 +1,17 @@
 package app.web;
 
+import app.jpa_repo.PublicChatMessageRepository;
 import app.jpa_repo.TextChannelRepository;
+import app.model.chat.PublicChatMessage;
 import app.model.chat.TextChannel;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasDynamicTitle;
 import com.vaadin.flow.router.HasUrlParameter;
@@ -13,11 +19,14 @@ import com.vaadin.flow.router.Route;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.Optional;
 
 @Route(value = "channels", layout = MainLayout.class)
 public class TextChannelView extends ComponentBuilder implements HasDynamicTitle, HasUrlParameter<Long> {
-    String textUser[] = {
+
+    String test[] = {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper sed ipsum nec vulputate. Duis gravida velit nec quam consequat semper. Vestibulum lobortis eros in dictum iaculis. Etiam mi est, rhoncus elementum leo a, maximus placerat odio. Nulla varius, lorem eleifend faucibus consequat, mi nulla placerat leo, nec suscipit nisl tellus vitae risus. Mauris a suscipit risus. Donec condimentum enim eu tortor euismod, eu porttitor libero mattis. Integer sodales, turpis vitae mollis mollis, felis mauris semper sem, in aliquet eros nisl et ante. Fusce vulputate tortor elit, et condimentum tortor dignissim vitae. Vivamus fermentum ultricies leo, ut maximus sem efficitur at. Vestibulum ultrices lacinia blandit. Mauris neque dui, varius ac magna non, tempus euismod erat. Aliquam laoreet pharetra faucibus. Proin consequat rhoncus diam, nec euismod felis gravida in.\n" +
                     "\n",
             "Quisque volutpat arcu vitae mi fringilla mattis. Cras eu massa neque. Morbi non dictum tellus. Nullam orci enim, gravida at felis sed, porttitor condimentum risus. Nam ac odio dolor. Donec sagittis sem ac ullamcorper aliquet. Aliquam erat volutpat. Mauris eros mauris, posuere vitae aliquet eu, hendrerit quis ex. Integer quam lorem, suscipit vitae ultrices sed, imperdiet non tortor.\n" +
@@ -29,24 +38,36 @@ public class TextChannelView extends ComponentBuilder implements HasDynamicTitle
     };
 
     private TextChannelRepository textChannelRepository;
+    private PublicChatMessageRepository publicMessageRepository;
+
     private TextChannel textChannel;
     //TODO: créer un objet Button img path(false/true) + statut
 
-    ComponentButton muteMicrophone;
-    ComponentButton muteHeadphone;
-    Button exitButton;
+    private ComponentButton muteMicrophone;
+    private ComponentButton muteHeadphone;
+
+    private Button sendMessage;
+    private Button exitButton;
+    private TextField textField = (TextField) textField();
+    private VerticalLayout LayoutTest = new VerticalLayout();
 
     //TODO: send message enter/button
     //TODO: sur la meme ligne
     //TODO: Change color loader
 
-    public TextChannelView(@Autowired TextChannelRepository textChannelRepository) {
+    public TextChannelView(@Autowired TextChannelRepository textChannelRepository, @Autowired PublicChatMessageRepository publicMessageRepository) {
         this.textChannelRepository = textChannelRepository;
+        this.publicMessageRepository = publicMessageRepository;
+
         muteMicrophone = new ComponentButton("img/micOn.svg", "img/micOff.svg", "unmute microphone", "mute microphone", Key.DIGIT_1);
-        muteMicrophone.addClickListener(event -> {muteMicrophone.changeStatus(event);});
+        muteMicrophone.addClickListener(event -> {
+            muteMicrophone.changeStatus(event);
+        });
 
         muteHeadphone = new ComponentButton("img/headsetOn.svg", "img/headsetOff.svg", "unmute headphone", "mute headphone", Key.DIGIT_2);
-        muteHeadphone.addClickListener(event -> {muteHeadphone.changeStatus(event);});
+        muteHeadphone.addClickListener(event -> {
+            muteHeadphone.changeStatus(event);
+        });
 
         exitButton = createButtonText("Quitter", "#F04747");
         exitButton.addClickListener(event -> {
@@ -54,12 +75,30 @@ public class TextChannelView extends ComponentBuilder implements HasDynamicTitle
             muteHeadphone.getStyle().set("display", "none");
             muteMicrophone.getStyle().set("display", "none");
         });
+        sendMessage = createButtonText("Envoyer", "#000");
+
+        sendMessage.addClickListener(event -> {
+            System.out.println("clique ici");
+            System.out.println(Date.from(Instant.now()).getTime() + " -------------------");
+            publicMessageRepository.save(PublicChatMessage.builder()
+                    .message(textField.getValue())
+                    .channelid(1L)
+                    .id(2L)
+                    .parentId(2L)
+                    .sender(0L)
+                    .timeCreated(Date.from(Instant.now()).getTime())
+                    .deleted(false)
+                    .build());
+            textField.focus();
+            textField.clear();
+            refreshZone();
+        });
         //exitButton.addClickListener(event -> ); hide mic and head
 
         createLayout(
-                createCard(ComponentBuilder.ColorHTML.DARKGRAY, "20%", "cardLeft", textUser),
-                createCard(ComponentBuilder.ColorHTML.GREY, "60%", "cardCenter", textUser, muteMicrophone, muteHeadphone, exitButton),
-                createCard(ComponentBuilder.ColorHTML.DARKGRAY, "20%", "cardRigth", textUser)
+                createCard(ComponentBuilder.ColorHTML.DARKGRAY, "20%", "cardLeft", test),
+                createCard(ComponentBuilder.ColorHTML.GREY, "60%", "cardCenter", test, sendMessage, muteMicrophone, muteHeadphone, exitButton),
+                createCard(ComponentBuilder.ColorHTML.DARKGRAY, "20%", "cardRigth", test)
         );
 
     }
@@ -93,4 +132,27 @@ public class TextChannelView extends ComponentBuilder implements HasDynamicTitle
             // TODO : take care of the exception
         }
     }
+
+
+    private void refreshZone() {
+        publicMessageRepository.findAll().stream()
+                .map(MessageLayout::new)
+                .forEach(LayoutTest::add);
+    }
+
+
+    class MessageLayout extends HorizontalLayout {
+        Text message = new Text("");
+
+        public MessageLayout(PublicChatMessage publicMessage) {
+            add(message);
+            Binder<PublicChatMessage> binder = new Binder<>(PublicChatMessage.class);
+            binder.bindInstanceFields(message);
+            binder.setBean(publicMessage);
+            binder.addValueChangeListener(event -> publicMessageRepository.save(binder.getBean()));
+
+        }
+
+    }
 }
+
