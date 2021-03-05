17816c3b05624e83a5ecf0525955e71edaddb561
Laure Runser <laure.runser@orange.fr>
2021-03-05 10:55:47 +0100

    Ft 30 31 modify moodle sections

--- src/main/java/app/jpa_repo/CourseSectionRepository.java
+++ src/main/java/app/jpa_repo/CourseSectionRepository.java
@@ -2,13 +2,22 @@ package app.jpa_repo;
 
 import app.model.courses.CourseSection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 public interface CourseSectionRepository extends JpaRepository<CourseSection, Long> {
 
-    // automatically creates the method to query the result
-    // (= we never have to implement any of the methods in this interface)
     ArrayList<CourseSection> findAllSectionsByCourseId(long id);
+
+    CourseSection findByParentId(long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE course_sections SET parentId = :new_id WHERE parentId = :old_id")
+    void updateParentId(@Param(value = "old_id") Long old_id, @Param(value = "new_id") Long new_id);
 
 }
