49335829aa682500eb041e33978beb4da39ba340
bouanem <bouanemyani2018@gmail.com>
2021-03-27 17:58:01 +0100

    fixing some errors

---
+++ src/main/java/app/web/views/PanelAdminView.java
@@ -0,0 +1,43 @@
+package app.web.views;
+
+import app.jpa_repo.PersonRepository;
+import app.model.users.Person;
+import app.web.layout.Navbar;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
+@Route(value = "admin", layout = Navbar.class)
+@PageTitle("adminView")
+
+public class PanelAdminView extends VerticalLayout {
+
+    private Grid<Person> grid = new Grid<>();
+    private PersonRepository personRepository;
+
+    public PanelAdminView(@Autowired PersonRepository personRepository) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Person sender = personRepository.findByUsername(username);
+        if(sender.getRole()==Person.Role.ADMIN) {
+            this.personRepository = personRepository;
+            grid.setItems(personRepository.findAll());
+            grid.addColumn(Person::getUsername).setHeader("Nom");
+            grid.addColumn(Person::getLastName).setHeader("LastName");
+            grid.addColumn(Person::getEmail).setHeader("Email");
+            grid.addColumn(Person::getRole).setHeader("Role");
+            grid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+                    GridVariant.LUMO_NO_ROW_BORDERS, GridVariant.LUMO_ROW_STRIPES);
+            add(grid);
+        }
+
+    }
+}
+
