395411845ebf7c80848740b030abf6162bc483bd
Laure Runser <laure-runser@tutanota.com>
2021-04-02 10:53:19 +0200

    refactor and clean up the code

--- src/main/java/app/web/views/PanelAdminView.java
+++ src/main/java/app/web/views/PanelAdminView.java
@@ -8,61 +8,68 @@ import app.web.layout.Navbar;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @Route(value = "admin", layout = Navbar.class)
-@PageTitle("adminView")
+@PageTitle("Discool : Admin Panel")
 
 public class PanelAdminView extends VerticalLayout {
 
-    private Grid<Person> grid = new Grid<>();
-    private Grid<Course> gridCourse = new Grid<>();
-    private PersonRepository personRepository;
-    private CourseRepository courseRepository;
+    private final PersonRepository personRepository;
+    private final CourseRepository courseRepository;
 
-    public PanelAdminView(@Autowired PersonRepository personRepository, CourseRepository courseRepository) {
+    private final Tab usersTab = new Tab("Users");
+    private final Tab coursesTab = new Tab("Courses");
+    private final Tabs tabs = new Tabs(usersTab, coursesTab);
+    private final Grid<Person> usersGrid = new Grid<>();
+    private final Grid<Course> coursesGrid = new Grid<>();
+
+    public PanelAdminView(@Autowired PersonRepository personRepository, @Autowired CourseRepository courseRepository) {
         this.personRepository = personRepository;
         this.courseRepository = courseRepository;
-        Tab users = new Tab("Users");
-        Tab courses = new Tab("Courses");
-        Tabs tabs = new Tabs(users, courses);
 
-        grid.setItems(personRepository.findAll());
-        grid.addColumn(Person::getUsername).setHeader("Nom");
-        grid.addColumn(Person::getLastName).setHeader("LastName");
-        grid.addColumn(Person::getEmail).setHeader("Email");
-        grid.addColumn(Person::getDescription).setHeader("description");
-        grid.addColumn(Person::getRole).setHeader("Role");
-        grid.addColumn(Person::getWebsite).setHeader("website");
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
-                GridVariant.LUMO_NO_ROW_BORDERS, GridVariant.LUMO_ROW_STRIPES);
+        createUserGird();
+        createCoursesGrid();
+        createTabs();
+    }
 
-        gridCourse.setItems(courseRepository.findAll());
-        gridCourse.addColumn(Course::getName).setHeader("Cours");
-        gridCourse.addColumn(Course::getTeacherId).setHeader("TeacherID");
-        gridCourse.addThemeVariants(GridVariant.LUMO_NO_BORDER,
-                GridVariant.LUMO_NO_ROW_BORDERS, GridVariant.LUMO_ROW_STRIPES);
-        users.add(grid);
-        courses.add(gridCourse);
-        tabs.add(users,courses);
+    private void createUserGird() {
+        usersGrid.setItems(personRepository.findAll());
+        usersGrid.addColumn(Person::getUsername).setHeader("Pseudo");
+        usersGrid.addColumn(Person::getLastName).setHeader("Last Name");
+        usersGrid.addColumn(Person::getFirstName).setHeader("First Name");
+        usersGrid.addColumn(Person::getEmail).setHeader("Email");
+        usersGrid.addColumn(Person::getDescription).setHeader("Description");
+        //usersGrid.addColumn(Person::getRole).setHeader("Role");
+        usersGrid.addColumn(Person::getWebsite).setHeader("Website");
+        usersGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+                              GridVariant.LUMO_NO_ROW_BORDERS, GridVariant.LUMO_ROW_STRIPES);
+        usersTab.add(usersGrid);
+    }
+
+    private void createCoursesGrid() {
+        coursesGrid.setItems(courseRepository.findAll());
+        coursesGrid.addColumn(Course::getName).setHeader("Name");
+        coursesGrid.addColumn(Course::getTeacherId).setHeader("TeacherID");
+        coursesGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER,
+                                     GridVariant.LUMO_NO_ROW_BORDERS, GridVariant.LUMO_ROW_STRIPES);
+        coursesTab.add(coursesGrid);
+    }
+
+    private void createTabs() {
+        tabs.add(usersTab,coursesTab);
 
         Map<Tab, Grid> tabsToPages = new HashMap<>();
-        tabsToPages.put(users, grid);
-        tabsToPages.put(courses, gridCourse);
+        tabsToPages.put(usersTab, usersGrid);
+        tabsToPages.put(coursesTab, coursesGrid);
         tabsToPages.values().forEach(page -> page.setVisible(false));
         Component selectedPage = tabsToPages.get(tabs.getSelectedTab());
         selectedPage.setVisible(true);
@@ -71,7 +78,7 @@ public class PanelAdminView extends VerticalLayout {
             Component insideSelectedPage = tabsToPages.get(tabs.getSelectedTab());
             insideSelectedPage.setVisible(true);
         });
-        add(tabs, grid,gridCourse);
+        add(tabs, usersGrid, coursesGrid);
     }
 }
 
