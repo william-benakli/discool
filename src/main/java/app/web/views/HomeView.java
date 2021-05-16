88cd16e96dad063df2d3284127adc8315aa0ccef
David Saad <david2maykel@gmail.com>
2021-05-16 02:04:30 +0200

    style HomeView

--- src/main/java/app/web/views/HomeView.java
+++ src/main/java/app/web/views/HomeView.java
@@ -4,6 +4,7 @@ import app.web.layout.Navbar;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -39,7 +40,7 @@ public class HomeView extends VerticalLayout {
                 new Paragraph("Un véritable encadrement scolaire"),
                 new Paragraph("Les Administrateurs et les professeurs sont la afin d'avoir le meme" +
                         " encadrement qu'en présentiel."),
-                new Image("img/Chat.svg", "alt")
+                new Image("img/Headhunter.svg", "alt")
         );
         imageLeft(
                 new Paragraph("Preserver le contacte humain"),
@@ -50,17 +51,96 @@ public class HomeView extends VerticalLayout {
     }
 
     public void imageLeft(Paragraph title, Paragraph text, Image image){
+        Div main = new Div();
+        Div inter = new Div();
+        Div left = new Div();
+        Div right = new Div();
+        Div interText = new Div();
 
-        this.add(title);
-        this.add(text);
-        this.add(image);
+        //Style Div
+        main.getStyle()
+                .set("height","100vh")
+                .set("width","100%")
+                .set("display","flex")
+                .set("margin","auto")
+                .set("border-bottom","solid 1px"+ ViewWithSidebars.ColorHTML.GREYTAB.getColorHtml());
+        inter.getStyle()
+                .set("display","flex")
+                .set("flex-direction","row")
+                .set("margin","auto")
+                .set("flex-wrap","wrap")
+                .set("align-content","center");
+        right.getStyle()
+                .set("display","flex")
+                .set("margin","auto");
+        left.getStyle().set("margin","auto");
+        title.getStyle()
+                .set("color", ViewWithSidebars.ColorHTML.PURPLE.getColorHtml())
+                .set("font-weight","700")
+                .set("font-size","20px");
+        text.getStyle()
+                .set("font-size","20px")
+                .set("text-align","justify");
+        interText.getStyle()
+                .set("max-width","350px")
+                .set("margin","auto");
+        image.getStyle()
+                .set("height","600px")
+                .set("width","600px");
+
+        interText.add(title, text);
+        left.add(image);
+
+        right.add(interText);
+        inter.add(left, right);
+        main.add(inter);
+        this.add(main);
     }
 
     public void imageRight(Paragraph title, Paragraph text, Image image){
+        Div main = new Div();
+        Div inter = new Div();
+        Div left = new Div();
+        Div right = new Div();
+        Div interText = new Div();
 
-        this.add(title);
-        this.add(text);
-        this.add(image);
+        //Style Div
+        main.getStyle()
+                .set("height","100vh")
+                .set("width","100%")
+                .set("display","flex")
+                .set("margin","auto")
+                .set("border-bottom","solid 1px"+ ViewWithSidebars.ColorHTML.GREYTAB.getColorHtml());
+        inter.getStyle()
+                .set("display","flex")
+                .set("flex-direction","row")
+                .set("margin","auto")
+                .set("flex-wrap","wrap");
+        left.getStyle()
+                .set("display","flex")
+                .set("margin","auto");
+        right.getStyle().set("margin","auto");
+        title.getStyle()
+                .set("color", ViewWithSidebars.ColorHTML.PURPLE.getColorHtml())
+                .set("font-weight","700")
+                .set("font-size","20px");
+        text.getStyle()
+                .set("font-size","20px")
+                .set("text-align","justify");
+        interText.getStyle()
+                .set("max-width","350px")
+                .set("margin","auto");
+        image.getStyle()
+                .set("height","600px")
+                .set("width","600px");
+
+        interText.add(title, text);
+        right.add(image);
+
+        left.add(interText);
+        inter.add(left, right);
+        main.add(inter);
+        this.add(main);
     }
 
 }
